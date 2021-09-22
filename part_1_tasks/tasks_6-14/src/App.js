4b6ff665de38b0fc37a42344f983f1222755320b
henna <henna.lehtinen91@gmail.com>
2021-09-22 21:08:19 +0300

    Did task 1.7: refactured the code to display also number of all feedback clicks, average score of feedback and percentage of positive feedback.

--- part_1_tasks/tasks_6-14/src/App.js
+++ part_1_tasks/tasks_6-14/src/App.js
@@ -10,15 +10,52 @@ const Display = ({text, counter}) => (
   <div>{text}: {counter}</div>
 )
 
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState([])
 
-  const handleGoodClick = () => {setGood(good + 1)}
-  const handleNeutralClick = () => {setNeutral(neutral + 1)}
-  const handleBadClick = () => {setBad(bad + 1)}
+  const handleGoodClick = () => {
+    setGood(good + 1) 
+    setAll(all + 1)
+    setAverage(average.concat(1))
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage(average.concat(0))
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    setAverage(average.concat(-1))
+  }
+
+  const countAverage = () => {
+    let averageSummed = 0;
+    if (average.length > 0) {
+      for (let i = 0; i < average.length; i++) {
+        averageSummed += average[i];
+      }
+      return averageSummed / average.length
+    }
+    else {
+      return 0
+    }
+  }
+
+  const countPositive = () => {
+    if (all > 0) {
+      return good / all * 100
+    }
+    else {
+      return 0
+    } 
+  }
 
   return (
     <div>
@@ -31,6 +68,9 @@ const App = () => {
       <Display text='good' counter={good}/>
       <Display text='neutral' counter={neutral}/>
       <Display text='bad' counter={bad}/>
+      <Display text='all' counter={all}/>
+      <Display text='average' counter ={countAverage()}/>
+      <Display text='positive %' counter={countPositive()}/>
     </div>
   )
 }
