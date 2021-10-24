0521b4f2a97dfff7141f4700e51f4aa6e1ac08b7
henna <henna.lehtinen91@gmail.com>
2021-10-24 21:34:00 +0300

    Did task 2.8: made it possible for adding phone numbers in addition to names. Used isNaN() to make sure the input number is actually a number.

--- part_2_tasks/tasks_6-10/src/App.js
+++ part_2_tasks/tasks_6-10/src/App.js
@@ -3,33 +3,47 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
   
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     // check if the name already exist in the phone book
     if (persons.find(person => person.name === personObject.name)) {
       window.alert(`${newName} is already added to the phonebook.`)  
-    } else {
-      // if the name doesn't exist yet let's add it to the phone book 
+    }
+    // check if the input phone number is really a number. Leaving the number blank is accepted. 
+    else if (isNaN(personObject.number) ) {
+      window.alert(`${newNumber} is not a valid phone number.`)
+    }
+    // if the person doesn't exist yet and the number is either a number or left blank let's add personObject to the phone book 
+    else { 
       setPersons(persons.concat(personObject))
       setNewName('')
-    }
-
-    
+      setNewNumber('')
+    } 
   }
+
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+
   const Person = ({person}) => {
     return (
-      <li>{person.name}</li>
+      <li>{person.name} {person.number}</li>
     )
   }
 
@@ -38,12 +52,9 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div> number: <input value ={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
