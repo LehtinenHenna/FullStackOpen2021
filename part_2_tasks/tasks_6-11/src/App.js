68a6d6f9d7481d14c0e55a6b9638ab718af3f837
henna <henna.lehtinen91@gmail.com>
2021-11-03 18:06:32 +0200

    Changed directory name

---
+++ part_2_tasks/tasks_6-11/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import {PersonForm, PersonsRender, Filter} from './components/Person.js'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+  const [ filter, setFilter ] = useState('')
+  
+  // fetching data from json server and saving that data to persons
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    // check if the name already exists in the phone book (case-insensitive)
+    if (persons.find(person => person.name.toUpperCase() === personObject.name.toUpperCase())) {
+      window.alert(`${newName} is already added to the phonebook.`)  
+    }
+    // check if the input phone number is really a number. Leaving the number blank is accepted. 
+    else if (isNaN(personObject.number)) {
+      window.alert(`${newNumber} is not a valid phone number.`)
+    }
+    // check that the name is not left blank
+    else if (personObject.name === '') {
+      window.alert(`Please add a name.`)
+    }
+    // if the person doesn't exist yet and the number is either a number or left blank let's add personObject to the phone book 
+    else { 
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    } 
+  }
+
+  const handleNameChange = (event) => {
+    // event.target.value contains whatever is written in the corresponding input box at the moment
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2> Phonebook </h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+      <h3> Add a new </h3>
+      <PersonForm handleAddPerson={handleAddPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      <h3> Numbers </h3>
+      <PersonsRender persons={persons} filter={filter} />   
+    </div>
+  )
+}
+
+export default App
